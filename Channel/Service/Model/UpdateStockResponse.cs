e87c7aea9b6bfe25d16cabb11b578f9a417a56bc
Arash <arash.zhianfard@gmail.com>
2022-03-20 12:47:14 +0100

    UpdateStockResponse added

---
+++ Channel/Service/Model/UpdateStockResponse.cs
@@ -0,0 +1,27 @@
+﻿using Service.Interfaces;
+using System.Text;
+
+namespace Service.Model
+{
+    public class UpdateStockResponse : BaseReponse<object>
+    {
+        public object Content { get; set; }
+        public int StatusCode { get; set; }
+        public int? LogId { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public Dictionary<string, List<string>> ValidationErrors { get; set; }
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("  Content: ").Append(Content).Append("\n");
+            stringBuilder.Append("  StatusCode: ").Append(StatusCode).Append("\n");
+            stringBuilder.Append("  LogId: ").Append(LogId).Append("\n");
+            stringBuilder.Append("  Success: ").Append(Success).Append("\n");
+            stringBuilder.Append("  Message: ").Append(Message).Append("\n");
+            stringBuilder.Append("  ValidationErrors: ").Append(ValidationErrors).Append("\n");
+            return stringBuilder.ToString();
+        }
+
+    }
+}
