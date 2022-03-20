3b323032414ebc9f98df3bdfebebeb1c860c6590
Arash <arash.zhianfard@gmail.com>
2022-03-20 12:40:53 +0100

    BaseReponse for all api reponses

---
+++ Channel/Service/Interfaces/BaseReponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Service.Interfaces
+{
+    public interface BaseReponse<T>
+    {
+        public T Content { get; set; }
+        public int StatusCode { get; set; }
+        public int? LogId { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public Dictionary<string, List<string>> ValidationErrors { get; set; }
+    }
+}
