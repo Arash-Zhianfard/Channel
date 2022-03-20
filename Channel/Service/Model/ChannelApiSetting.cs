3e5b5f34650457b26e61d6dfd3b3e4fab55bbae3
Arash <arash.zhianfard@gmail.com>
2022-03-20 12:39:48 +0100

    appsetting for endpoint and apis

---
+++ Channel/Service/Model/ChannelApiSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.Model
+{
+    public class ChannelApiSetting
+    {
+        public string BaseUrl { get; set; }
+        public string Token { get; set; }
+        public string Order { get; set; }
+        public string Offer { get; set; }
+
+    }
+}
