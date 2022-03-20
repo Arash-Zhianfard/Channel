5eb33b1891f05d267921f9c16c2101e1c7d70f22
Arash <arash.zhianfard@gmail.com>
2022-03-21 00:10:17 +0100

    IStockCharger added to controller

--- Channel/WebApp/Controllers/OfferController.cs
+++ Channel/WebApp/Controllers/OfferController.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Service.Implementation;
+using Service.Interfaces;
 
 namespace WebApp.Controllers
 {
     public class OfferController : Controller
     {
-        private readonly IOfferApiSync _offerApiSync;
-        public OfferController(IOfferApiSync offerApiSync)
+        private readonly IStockCharger _stockCharger;
+        public OfferController(IStockCharger stockCharger)
         {
-            _offerApiSync = offerApiSync;
+            _stockCharger = stockCharger;
         }
         public async Task<IActionResult> Index()
         {
-            var result = await _offerApiSync.UpdateStockCountAsync();
+            var result = await _stockCharger.UpdateStockCountAsync();
             return View(result);
         }
     }
