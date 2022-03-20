bc2c64531a9267fd3fe1f8380c9dda764aae0341
Arash <arash.zhianfard@gmail.com>
2022-03-20 15:37:10 +0100

    offer contoller and view added

---
+++ Channel/WebApp/Controllers/OfferController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Implementation;
+
+namespace WebApp.Controllers
+{
+    public class OfferController : Controller
+    {
+        private readonly IOfferApiSync _offerApiSync;
+        public OfferController(IOfferApiSync offerApiSync)
+        {
+            _offerApiSync = offerApiSync;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _offerApiSync.UpdateStockCountAsync();
+            return View(result);
+        }
+    }
+}
