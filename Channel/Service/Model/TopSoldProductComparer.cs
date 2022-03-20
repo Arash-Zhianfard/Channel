590e0c585f11ccb3b52ab24b9df4179be141c8ba
Arash <arash.zhianfard@gmail.com>
2022-03-20 13:13:01 +0100

    top 5 sold Product added

---
+++ Channel/Service/Model/TopSoldProductComparer.cs
@@ -0,0 +1,27 @@
+﻿namespace Service.Model
+{
+    public class TopSoldProductComparer : IEqualityComparer<TopSoldProduct>
+    {
+        public new bool Equals(TopSoldProduct? x, TopSoldProduct? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Gtin == y.Gtin && Equals(x.TotalQuantity, y.TotalQuantity)
+               && x.Description == y.Description;
+        }
+
+        public int GetHashCode(TopSoldProduct obj)
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + obj.GetHashCode();
+                hash = hash * 23 + obj.GetHashCode();
+                hash = hash * 23 + obj.GetHashCode();
+                return hash;
+            }
+        }
+    }
+}
--- Channel/Tests/UnitTest1.cs
+++ Channel/Tests/UnitTest1.cs
@@ -1,18 +1,124 @@
+using ChannelEngine.Merchant.ApiClient.Api;
+using ChannelEngine.Merchant.ApiClient.Model;
+using Microsoft.Extensions.Options;
+using Moq;
 using NUnit.Framework;
+using Service.Implementation;
+using Service.Interfaces;
+using Service.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Tests
+namespace ChannelEngineTest
 {
-    public class Tests
+    public class ProductTest
     {
+        private Mock<IOrderApiAsync> _orderApiAsyncMock;
         [SetUp]
         public void Setup()
         {
+            _orderApiAsyncMock = new Mock<IOrderApiAsync>();
+            _orderApiAsyncMock.Setup(x => x.OrderGetByFilterAsync(It.IsAny<OrderFilterOption>()))
+            .Returns(Task.FromResult(DummyOrderResponse()));
         }
 
-        [Test]
-        public void Test1()
+        private static ProductOrderResponse DummyOrderResponse()
         {
-            Assert.Pass();
+            var orderReposne = new ProductOrderResponse();
+            orderReposne.Success = true;
+            orderReposne.Count = 6;
+            orderReposne.Content = new List<MerchantOrderResponse>();
+            orderReposne.Content.Add(new MerchantOrderResponse()
+            {
+                Id = 1,
+                Lines = new List<MerchantOrderLineResponse>
+                     {
+                         new MerchantOrderLineResponse()
+                         {
+                             Description = "product 1",
+                             MerchantProductNo="1234",
+                             Gtin = "1212",
+                             Quantity = 2
+                         }}
+            });
+            orderReposne.Content.Add(new MerchantOrderResponse()
+            {
+                Id = 2,
+                Lines = new List<MerchantOrderLineResponse>
+                        {
+                         new MerchantOrderLineResponse()
+                         {
+                             Description = "product 1",
+                             MerchantProductNo="1234",
+                             Gtin = "1212",
+                             Quantity = 1
+                         }}
+            });
+            orderReposne.Content.Add(new MerchantOrderResponse()
+            {
+                Id = 1,
+                Lines = new List<MerchantOrderLineResponse>
+                        {
+                            new MerchantOrderLineResponse()
+                         {
+                             Description = "product 2",
+                             MerchantProductNo="321",
+                             Gtin = "1212",
+                             Quantity = 2
+                         }}
+            });
+            orderReposne.Content.Add(new MerchantOrderResponse()
+            {
+                Id = 1,
+                Lines = new List<MerchantOrderLineResponse>
+                        {
+                            new MerchantOrderLineResponse()
+                         {
+                             Description = "product 2",
+                             MerchantProductNo="321",
+                             Gtin = "1212",
+                             Quantity = 2
+                         }}
+            });
+            orderReposne.Content.Add(new MerchantOrderResponse()
+            {
+                Id = 1,
+                Lines = new List<MerchantOrderLineResponse>
+                        {
+                            new MerchantOrderLineResponse()
+                         {
+                             Description = "product 3",
+                             MerchantProductNo="258",
+                             Gtin = "12121",
+                             Quantity = 2
+                         }}
+            });
+            orderReposne.Content.Add(new MerchantOrderResponse()
+            {
+                Id = 1,
+                Lines = new List<MerchantOrderLineResponse>
+                        {
+                            new MerchantOrderLineResponse()
+                         {
+                             Description = "product 4",
+                             MerchantProductNo="159",
+                             Gtin = "753",
+                             Quantity = 1
+                         }}
+            });
+            return orderReposne;
+        }
+        [Test]
+        public void TestTopNSoldProduct()
+        {
+            ProductAsync productAsync = new ProductAsync(_orderApiAsyncMock.Object);
+            List<TopSoldProduct> expectedProduct = new List<TopSoldProduct>();
+            expectedProduct.Add(new TopSoldProduct("1212", "product 2", 4));
+            expectedProduct.Add(new TopSoldProduct("1212", "product 1", 3));
+            expectedProduct.Add(new TopSoldProduct("12121", "product 3", 2));
+            expectedProduct.Add(new TopSoldProduct("753", "product 4", 1));
+            var result = productAsync.GetTopSoldProduct(5).Result;
+            Assert.That(result, Is.EqualTo(expectedProduct).Using(new TopSoldProductComparer()));
         }
     }
 }
