b87fe3481b9b0d6db853866082322ea271400c74
Arash <arash.zhianfard@gmail.com>
2022-03-20 13:04:32 +0100

    console setup

--- Channel/Channel/Program.cs
+++ Channel/Channel/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        services
+            .AddSingleton<Executor, Executor>()
+            .BuildServiceProvider()
+            .GetService<Executor>()
+            .Execute();
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false)
+        .AddEnvironmentVariables()
+        .Build();
+    }
+}
+
+public class Executor
+{
+
+
+    public void Execute()
+    {
+       
+        Console.ReadKey();
+    }
+}
